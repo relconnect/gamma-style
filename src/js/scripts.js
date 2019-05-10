9c9526d8abef56d566d15e74a2dfd432c5871c2f
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2019-05-10 18:31:27 +0300

    add category page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $("#responsiveTabs").responsiveTabs({
     startCollapsed: "accordion"
   });
@@ -12,21 +12,21 @@ $(document).ready(function() {
     verticalSwiping: true
   });
 
-  $(".r-tabs-tab").click(function() {
+  $(".r-tabs-tab").click(function () {
     $(".bx-slider").slick("setPosition");
   });
 
-  $(".stock-compare__link").click(function(e) {
+  $(".stock-compare__link").click(function (e) {
     e.preventDefault();
     $(this).toggleClass("active");
   });
 
   // Плавное появление текста на главной
-  $(function() {
+  $(function () {
     var nav = $(".text-content__wrap"),
       animateTime = 500,
       navLink = $(".text-content__more-btn");
-    navLink.click(function() {
+    navLink.click(function () {
       if (nav.height() === 170) {
         autoHeightAnimate(nav, animateTime);
         nav.removeClass("hide-text");
@@ -50,8 +50,8 @@ $(document).ready(function() {
   });
 
   //Открыть/закрыть выпадалку с телефонами
-  $(function() {
-    $(".arrow").click(function() {
+  $(function () {
+    $(".arrow").click(function () {
       $(this)
         .closest(".dropdown")
         .toggleClass("open");
@@ -59,7 +59,7 @@ $(document).ready(function() {
   });
 
   //Скрываем блок с телефонами  при клике за пределами блока
-  $(document).on("click", function(e) {
+  $(document).on("click", function (e) {
     if (
       !$(e.target).closest($(".dropdown")).length &&
       $(".dropdown").hasClass("open")
@@ -69,23 +69,23 @@ $(document).ready(function() {
     e.stopPropagation();
   });
 
-  $(".form__input").focus(function() {
+  $(".form__input").focus(function () {
     $(this)
       .siblings(".form__label")
       .addClass("visible");
   });
-  $(".form__message").focus(function() {
+  $(".form__message").focus(function () {
     $(this)
       .siblings(".form__label")
       .addClass("visible");
   });
 
-  $(".form__input").blur(function() {
+  $(".form__input").blur(function () {
     $(this)
       .siblings(".form__label")
       .removeClass("visible");
   });
-  $(".form__message").blur(function() {
+  $(".form__message").blur(function () {
     $(this)
       .siblings(".form__label")
       .removeClass("visible");
@@ -93,11 +93,11 @@ $(document).ready(function() {
 
   //Меняем цвет активной кнопки для изменения отображения товаров в каталоге
 
-  $(function() {
-    $(".mode__link").click(function(e) {
+  $(function () {
+    $(".mode__link").click(function (e) {
       e.preventDefault();
 
-      $(".mode__link").each(function(index, elem) {
+      $(".mode__link").each(function (index, elem) {
         $(elem).removeClass("active");
       });
       $(e.target).addClass("active");
@@ -105,8 +105,8 @@ $(document).ready(function() {
   });
 
   // Show/hide  параметры фильтра в каталоге
-  $(function() {
-    $(".filter_block-name .filter_img").click(function(e) {
+  $(function () {
+    $(".filter_block-name .filter_img").click(function (e) {
       let img = $(this);
       let parent = img.closest(".filter_block");
       img.toggleClass("show");
@@ -119,23 +119,24 @@ $(document).ready(function() {
   });
 
   //Показать/скрыть дополнительное меню вторго уровня в каталоге
-  $(function() {
-    $(".sub-menu__item").hover(function(e) {
+  $(function () {
+    $(".sub-menu__item").hover(function (e) {
+      console.log(1)
       let item = $(this);
       let link = item.find(".sub-menu__link");
       link.toggleClass("show");
       if ($(link).hasClass("show")) {
         $(link)
-          .next(".sum-menu__list-second")
+          .next(".sub-menu__list-second")
           .show();
       } else {
-        link.next(".sum-menu__list-second").hide();
+        link.next(".sub-menu__list-second").hide();
       }
     });
   });
 
-  $(function() {
-    $(".popup-menu-link").click(function(e) {
+  $(function () {
+    $(".popup-menu-link").click(function (e) {
       e.preventDefault();
       $(this).toggleClass("isShown");
 
@@ -148,31 +149,31 @@ $(document).ready(function() {
   });
 });
 //Настройка range слайдера
-$(document).ready(function() {
+$(document).ready(function () {
   $("#slider").slider({
     min: 0,
     max: 5000,
     step: 1,
     range: true,
     values: [0, 5000],
-    slide: function(event, ui) {
+    slide: function (event, ui) {
       for (var i = 0; i < ui.values.length; ++i) {
         $("input.sliderValue[data-index=" + i + "]").val(ui.values[i]);
       }
     }
   });
 
-  $("input.sliderValue").change(function() {
+  $("input.sliderValue").change(function () {
     var $this = $(this);
     $("#slider").slider("values", $this.data("index"), $this.val());
   });
 
   // Плавное появление списка фильтров в каталоге
-  $(function() {
+  $(function () {
     var nav = $(".more-filter__btn").prev(),
       animateTime = 500,
       navLink = $(".more-filter__btn");
-    navLink.click(function(e) {
+    navLink.click(function (e) {
       if (nav.height() === 164) {
         autoHeightAnimate(nav, animateTime);
         nav.removeClass("hide");
@@ -197,7 +198,7 @@ $(document).ready(function() {
 });
 
 //Скрываем дополнительное меню при клике за пределами блока
-$(document).on("click", function(e) {
+$(document).on("click", function (e) {
   if (
     !$(e.target).closest($(".popup-menu-link")).length &&
     $(".popup-menu-link").hasClass("isShown")
@@ -206,4 +207,21 @@ $(document).on("click", function(e) {
     $(".pop-up__submenu").hide();
   }
   e.stopPropagation();
+});
+
+
+$(function () {
+  $(".cat__block").hover(function (e) {
+    console.log(1)
+    let item = $(this);
+    let link = item.find(".promo");
+    link.toggleClass("show");
+    if ($(link).hasClass("show")) {
+      $(link)
+        .find(".pop-menu__list")
+        .show();
+    } else {
+      link.find(".pop-menu__list").hide();
+    }
+  });
 });
