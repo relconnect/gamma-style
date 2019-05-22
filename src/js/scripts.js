264bfe26fb6d974ea16344d79844bdc6507fd343
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2019-05-22 19:32:26 +0300

    1366 completed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $("#responsiveTabs").responsiveTabs({
     startCollapsed: "accordion"
   });
@@ -9,11 +9,11 @@ $(document).ready(function () {
     startCollapsed: "accordion"
   });
   // Плавное появление текста на главной
-  $(function () {
+  $(function() {
     var nav = $(".text-content__wrap"),
       animateTime = 500,
       navLink = $(".text-content__more-btn");
-    navLink.click(function () {
+    navLink.click(function() {
       if (nav.height() === 170) {
         autoHeightAnimate(nav, animateTime);
         nav.removeClass("hide-text");
@@ -53,15 +53,14 @@ $(document).ready(function () {
           dots: true,
           autoplay: false,
           autoplaySpeed: 6000,
-          verticalSwiping: true,
+          verticalSwiping: true
         }
-      },
+      }
 
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
     ]
-
   });
 
   $(".bx-slider-index").slick({
@@ -70,10 +69,8 @@ $(document).ready(function () {
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 6000,
-    verticalSwiping: true,
-
+    verticalSwiping: true
   });
-
 
   $("#recommendation-slider").slick({
     adaptiveHeight: true,
@@ -81,7 +78,25 @@ $(document).ready(function () {
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 6000,
-    verticalSwiping: true
+    verticalSwiping: true,
+    responsive: [
+      {
+        breakpoint: 1367,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: false,
+          autoplaySpeed: 6000,
+          verticalSwiping: true
+        }
+      }
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
   $(".bs-slider").slick({
@@ -90,9 +105,26 @@ $(document).ready(function () {
     slidesToScroll: 1,
     autoplay: false,
     autoplaySpeed: 6000,
-    verticalSwiping: true
-  });
+    verticalSwiping: true,
+    responsive: [
+      {
+        breakpoint: 1367,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: false,
+          autoplaySpeed: 6000,
+          verticalSwiping: true
+        }
+      }
 
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
 
   $("#product-action-slider").slick({
     adaptiveHeight: true,
@@ -103,24 +135,22 @@ $(document).ready(function () {
     verticalSwiping: true
   });
 
-  $(".r-tabs-tab").click(function () {
+  $(".r-tabs-tab").click(function() {
     $(".bx-slider").slick("setPosition");
   });
 
-  $(".r-tabs-tab").click(function () {
+  $(".r-tabs-tab").click(function() {
     $(".bs-slider").slick("setPosition");
   });
 
-  $(".stock-compare__link").click(function (e) {
+  $(".stock-compare__link").click(function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
   });
 
-
-
   //Открыть/закрыть выпадалку с телефонами
-  $(function () {
-    $(".arrow").click(function () {
+  $(function() {
+    $(".arrow").click(function() {
       $(this)
         .closest(".dropdown")
         .toggleClass("open");
@@ -128,30 +158,30 @@ $(document).ready(function () {
   });
 
   //Скрываем блок с телефонами  при клике за пределами блока
-  $(document).on("click", function (e) {
+  $(document).on("click", function(e) {
     if (!$(e.target).closest($(".dropdown")).length && $(".dropdown").hasClass("open")) {
       $(".dropdown").removeClass("open");
     }
     e.stopPropagation();
   });
 
-  $(".form__input").focus(function () {
+  $(".form__input").focus(function() {
     $(this)
       .siblings(".form__label")
       .addClass("visible");
   });
-  $(".form__message").focus(function () {
+  $(".form__message").focus(function() {
     $(this)
       .siblings(".form__label")
       .addClass("visible");
   });
 
-  $(".form__input").blur(function () {
+  $(".form__input").blur(function() {
     $(this)
       .siblings(".form__label")
       .removeClass("visible");
   });
-  $(".form__message").blur(function () {
+  $(".form__message").blur(function() {
     $(this)
       .siblings(".form__label")
       .removeClass("visible");
@@ -159,11 +189,11 @@ $(document).ready(function () {
 
   //Меняем цвет активной кнопки для изменения отображения товаров в каталоге
 
-  $(function () {
-    $(".mode__link").click(function (e) {
+  $(function() {
+    $(".mode__link").click(function(e) {
       e.preventDefault();
 
-      $(".mode__link").each(function (index, elem) {
+      $(".mode__link").each(function(index, elem) {
         $(elem).removeClass("active");
       });
       $(e.target).addClass("active");
@@ -171,8 +201,8 @@ $(document).ready(function () {
   });
 
   // Show/hide  параметры фильтра в каталоге
-  $(function () {
-    $(".filter_block-name .filter_img").click(function (e) {
+  $(function() {
+    $(".filter_block-name .filter_img").click(function(e) {
       let img = $(this);
       let parent = img.closest(".filter_block");
       img.toggleClass("show");
@@ -185,8 +215,8 @@ $(document).ready(function () {
   });
 
   //Показать/скрыть дополнительное меню вторго уровня в каталоге
-  $(function () {
-    $(".sub-menu__item").hover(function (e) {
+  $(function() {
+    $(".sub-menu__item").hover(function(e) {
       console.log(1);
       let item = $(this);
       let link = item.find(".sub-menu__link");
@@ -201,8 +231,8 @@ $(document).ready(function () {
     });
   });
 
-  $(function () {
-    $(".popup-menu-link").click(function (e) {
+  $(function() {
+    $(".popup-menu-link").click(function(e) {
       e.preventDefault();
       $(this).toggleClass("isShown");
 
@@ -215,31 +245,31 @@ $(document).ready(function () {
   });
 });
 //Настройка range слайдера
-$(document).ready(function () {
+$(document).ready(function() {
   $("#slider").slider({
     min: 0,
     max: 5000,
     step: 1,
     range: true,
     values: [0, 5000],
-    slide: function (event, ui) {
+    slide: function(event, ui) {
       for (var i = 0; i < ui.values.length; ++i) {
         $("input.sliderValue[data-index=" + i + "]").val(ui.values[i]);
       }
     }
   });
 
-  $("input.sliderValue").change(function () {
+  $("input.sliderValue").change(function() {
     var $this = $(this);
     $("#slider").slider("values", $this.data("index"), $this.val());
   });
 
   // Плавное появление списка фильтров в каталоге
-  $(function () {
+  $(function() {
     var nav = $(".more-filter__btn").prev(),
       animateTime = 500,
       navLink = $(".more-filter__btn");
-    navLink.click(function (e) {
+    navLink.click(function(e) {
       if (nav.height() === 164) {
         autoHeightAnimate(nav, animateTime);
         nav.removeClass("hide");
@@ -264,7 +294,7 @@ $(document).ready(function () {
 });
 
 //Скрываем дополнительное меню при клике за пределами блока
-$(document).on("click", function (e) {
+$(document).on("click", function(e) {
   if (!$(e.target).closest($(".popup-menu-link")).length && $(".popup-menu-link").hasClass("isShown")) {
     $(".popup-menu-link").removeClass("isShown");
     $(".pop-up__submenu").hide();
@@ -272,9 +302,9 @@ $(document).on("click", function (e) {
   e.stopPropagation();
 });
 
-$(function () {
+$(function() {
   $(".cat__block").hover(
-    function (e) {
+    function(e) {
       let item = $(this);
       item.addClass("hovered");
       let link = item.find(".promo");
@@ -287,7 +317,7 @@ $(function () {
         link.find(".pop-menu__list").hide();
       }
     },
-    function () {
+    function() {
       let item = $(this);
       item.addClass("hovered");
       let link = item.find(".promo");
@@ -311,31 +341,57 @@ $(function () {
   });
 });
 
-$(function () {
-  $(".rating__customer").each(function () {
-    console.log($(this))
-    $(this).barrating({
-      theme: "css-stars", readonly: true
+if ($(".rating__customer").length) {
+  $(function() {
+    $(".rating__customer").each(function() {
+      $(this).barrating({
+        theme: "css-stars",
+        readonly: true
+      });
     });
-  })
-});
-$(function () {
-  $("#from-rating__customer").barrating({
-    theme: "css-stars"
+  });
+}
+
+if ($(".rating__customer").length) {
+  $(function() {
+    $("#from-rating__customer").barrating({
+      theme: "css-stars"
+    });
+  });
+}
+
+$(function() {
+  $(".show__review-btn").click(function() {
+    $(".review_clip").show(300);
+    $(this).hide(200);
   });
 });
 
-$(function () {
-  $('.show__review-btn').click(function () {
-    $('.review_clip').show(300);
-    $(this).hide(200);
-  })
-})
-
-$(function () {
-  $('.plus, .minus').click(function (evt) {
-    let input = $(this).closest('.product__quantity').find('input').val();
-    // var inp = $(this).closest('.num').find('input').val();
-    $(this).hasClass('minus') ? $(this).closest('.product__quantity').find('input').val(input * 1 - 1) : $(this).closest('.product__quantity').find('input').val(input * 1 + 1);
-  })
-})
+$(function() {
+  $(".plus, .minus").click(function(evt) {
+    let input = $(this)
+      .closest(".product__quantity")
+      .find("input")
+      .val();
+    if (input > 0) {
+      $(this).hasClass("minus")
+        ? $(this)
+            .closest(".product__quantity")
+            .find("input")
+            .val(input * 1 - 1)
+        : $(this)
+            .closest(".product__quantity")
+            .find("input")
+            .val(input * 1 + 1);
+    } else if (input == 0) {
+      $(this).hasClass("minus")
+        ? false
+        : $(this)
+            .closest(".product__quantity")
+            .find("input")
+            .val(input * 1 + 1);
+    } else {
+      return;
+    }
+  });
+});
