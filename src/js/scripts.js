a7c6b394feec3b0714ce0c5d84ddc36216ffd8af
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2019-05-25 18:22:59 +0300

    768 start

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $("#responsiveTabs").responsiveTabs({
     startCollapsed: "accordion"
   });
@@ -9,11 +9,11 @@ $(document).ready(function () {
     startCollapsed: "accordion"
   });
   // Плавное появление текста на главной
-  $(function () {
+  $(function() {
     var nav = $(".text-content__wrap"),
       animateTime = 500,
       navLink = $(".text-content__more-btn");
-    navLink.click(function () {
+    navLink.click(function() {
       if (nav.height() === 170) {
         autoHeightAnimate(nav, animateTime);
         nav.removeClass("hide-text");
@@ -75,6 +75,18 @@ $(document).ready(function () {
         breakpoint: 1025,
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          autoplay: false,
+          autoplaySpeed: 6000,
+          verticalSwiping: true
+        }
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 2,
           slidesToScroll: 1,
           infinite: true,
           dots: true,
@@ -148,7 +160,8 @@ $(document).ready(function () {
           autoplaySpeed: 6000,
           verticalSwiping: true
         }
-      }, {
+      },
+      {
         breakpoint: 1027,
         settings: {
           slidesToShow: 3,
@@ -176,22 +189,22 @@ $(document).ready(function () {
   //   verticalSwiping: true
   // });
 
-  $(".r-tabs-tab").click(function () {
+  $(".r-tabs-tab").click(function() {
     $(".bx-slider").slick("setPosition");
   });
 
-  $(".r-tabs-tab").click(function () {
+  $(".r-tabs-tab").click(function() {
     $(".bs-slider").slick("setPosition");
   });
 
-  $(".stock-compare__link").click(function (e) {
+  $(".stock-compare__link").click(function(e) {
     e.preventDefault();
     $(this).toggleClass("active");
   });
 
   //Открыть/закрыть выпадалку с телефонами
-  $(function () {
-    $(".arrow").click(function () {
+  $(function() {
+    $(".arrow").click(function() {
       $(this)
         .closest(".dropdown")
         .toggleClass("open");
@@ -199,30 +212,30 @@ $(document).ready(function () {
   });
 
   //Скрываем блок с телефонами  при клике за пределами блока
-  $(document).on("click", function (e) {
+  $(document).on("click", function(e) {
     if (!$(e.target).closest($(".dropdown")).length && $(".dropdown").hasClass("open")) {
       $(".dropdown").removeClass("open");
     }
     e.stopPropagation();
   });
 
-  $(".form__input").focus(function () {
+  $(".form__input").focus(function() {
     $(this)
       .siblings(".form__label")
       .addClass("visible");
   });
-  $(".form__message").focus(function () {
+  $(".form__message").focus(function() {
     $(this)
       .siblings(".form__label")
       .addClass("visible");
   });
 
-  $(".form__input").blur(function () {
+  $(".form__input").blur(function() {
     $(this)
       .siblings(".form__label")
       .removeClass("visible");
   });
-  $(".form__message").blur(function () {
+  $(".form__message").blur(function() {
     $(this)
       .siblings(".form__label")
       .removeClass("visible");
@@ -230,11 +243,11 @@ $(document).ready(function () {
 
   //Меняем цвет активной кнопки для изменения отображения товаров в каталоге
 
-  $(function () {
-    $(".mode__link").click(function (e) {
+  $(function() {
+    $(".mode__link").click(function(e) {
       e.preventDefault();
 
-      $(".mode__link").each(function (index, elem) {
+      $(".mode__link").each(function(index, elem) {
         $(elem).removeClass("active");
       });
       $(e.target).addClass("active");
@@ -242,8 +255,8 @@ $(document).ready(function () {
   });
 
   // Show/hide  параметры фильтра в каталоге
-  $(function () {
-    $(".filter_block-name .filter_img").click(function (e) {
+  $(function() {
+    $(".filter_block-name .filter_img").click(function(e) {
       let img = $(this);
       let parent = img.closest(".filter_block");
       img.toggleClass("show");
@@ -256,8 +269,8 @@ $(document).ready(function () {
   });
 
   //Показать/скрыть дополнительное меню вторго уровня в каталоге
-  $(function () {
-    $(".sub-menu__item").hover(function (e) {
+  $(function() {
+    $(".sub-menu__item").hover(function(e) {
       console.log(1);
       let item = $(this);
       let link = item.find(".sub-menu__link");
@@ -272,8 +285,8 @@ $(document).ready(function () {
     });
   });
 
-  $(function () {
-    $(".popup-menu-link").click(function (e) {
+  $(function() {
+    $(".popup-menu-link").click(function(e) {
       e.preventDefault();
       $(this).toggleClass("isShown");
 
@@ -286,31 +299,31 @@ $(document).ready(function () {
   });
 });
 //Настройка range слайдера
-$(document).ready(function () {
+$(document).ready(function() {
   $("#slider").slider({
     min: 0,
     max: 5000,
     step: 1,
     range: true,
     values: [0, 5000],
-    slide: function (event, ui) {
+    slide: function(event, ui) {
       for (var i = 0; i < ui.values.length; ++i) {
         $("input.sliderValue[data-index=" + i + "]").val(ui.values[i]);
       }
     }
   });
 
-  $("input.sliderValue").change(function () {
+  $("input.sliderValue").change(function() {
     var $this = $(this);
     $("#slider").slider("values", $this.data("index"), $this.val());
   });
 
   // Плавное появление списка фильтров в каталоге
-  $(function () {
+  $(function() {
     var nav = $(".more-filter__btn").prev(),
       animateTime = 500,
       navLink = $(".more-filter__btn");
-    navLink.click(function (e) {
+    navLink.click(function(e) {
       if (nav.height() === 164) {
         autoHeightAnimate(nav, animateTime);
         nav.removeClass("hide");
@@ -335,7 +348,7 @@ $(document).ready(function () {
 });
 
 //Скрываем дополнительное меню при клике за пределами блока
-$(document).on("click", function (e) {
+$(document).on("click", function(e) {
   if (!$(e.target).closest($(".popup-menu-link")).length && $(".popup-menu-link").hasClass("isShown")) {
     $(".popup-menu-link").removeClass("isShown");
     $(".pop-up__submenu").hide();
@@ -343,9 +356,9 @@ $(document).on("click", function (e) {
   e.stopPropagation();
 });
 
-$(function () {
+$(function() {
   $(".cat__block").hover(
-    function (e) {
+    function(e) {
       let item = $(this);
       item.addClass("hovered");
       let link = item.find(".promo");
@@ -358,7 +371,7 @@ $(function () {
         link.find(".pop-menu__list").hide();
       }
     },
-    function () {
+    function() {
       let item = $(this);
       item.addClass("hovered");
       let link = item.find(".promo");
@@ -383,8 +396,8 @@ $(function () {
 });
 
 if ($(".rating__customer").length) {
-  $(function () {
-    $(".rating__customer").each(function () {
+  $(function() {
+    $(".rating__customer").each(function() {
       $(this).barrating({
         theme: "css-stars",
         readonly: true
@@ -394,22 +407,22 @@ if ($(".rating__customer").length) {
 }
 
 if ($(".rating__customer").length) {
-  $(function () {
+  $(function() {
     $("#from-rating__customer").barrating({
       theme: "css-stars"
     });
   });
 }
 
-$(function () {
-  $(".show__review-btn").click(function () {
+$(function() {
+  $(".show__review-btn").click(function() {
     $(".review_clip").show(300);
     $(this).hide(200);
   });
 });
 
-$(function () {
-  $(".plus, .minus").click(function (evt) {
+$(function() {
+  $(".plus, .minus").click(function(evt) {
     let input = $(this)
       .closest(".product__quantity")
       .find("input")
@@ -417,20 +430,20 @@ $(function () {
     if (input > 0) {
       $(this).hasClass("minus")
         ? $(this)
-          .closest(".product__quantity")
-          .find("input")
-          .val(input * 1 - 1)
+            .closest(".product__quantity")
+            .find("input")
+            .val(input * 1 - 1)
         : $(this)
-          .closest(".product__quantity")
-          .find("input")
-          .val(input * 1 + 1);
+            .closest(".product__quantity")
+            .find("input")
+            .val(input * 1 + 1);
     } else if (input == 0) {
       $(this).hasClass("minus")
         ? false
         : $(this)
-          .closest(".product__quantity")
-          .find("input")
-          .val(input * 1 + 1);
+            .closest(".product__quantity")
+            .find("input")
+            .val(input * 1 + 1);
     } else {
       return;
     }
